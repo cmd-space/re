5bd01037ef71cd24c9d9bfcd29b194ce9d437fec
Mason Crane <masonecrane@gmail.com>
2015-07-08 14:43:31 -0700

    moved partial to index.html, worked out issue with dropdown not functioning properly

--- client/js/main.js
+++ client/js/main.js
@@ -10,6 +10,28 @@ reApp.config(function($routeProvider) {
 		});
 });
 
+angular.module('reApp').controller('DropdownCtrl', function ($scope, $log) {
+	// $scope.items = [
+	// 'The first choice!',
+	// 'And another choice for you.',
+	// 'but wait! A third!'
+	// ];
+
+	$scope.status = {
+		isopen: false
+	};
+
+	$scope.toggled = function(open) {
+		$log.log('Dropdown is now: ', open);
+	};
+
+	$scope.toggleDropdown = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.status.isopen = !$scope.status.isopen;
+	};
+});
+
 reApp.controller('calcsController', function($scope) {
 
 });
